389960695e5e2412367836a92b08e0f62f4b145b
Ozan Andaç <oynozan@hotmail.com>
2026-01-14 15:29:07 +0100

    empty boilerplate

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Coset</span>,
+  project: {
+    link: 'https://github.com/CosetDev',
+  },
+  footer: {
+    text: 'Coset - Public Oracle Ecosystem',
+  },
+}
+
+export default config
