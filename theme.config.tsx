7d9ae631831772b0daa7cc52071cdfa02105fe45
Ozan Andaç <oynozan@hotmail.com>
2026-01-16 10:08:20 +0100

    Completed documentation of Coset MVP version

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Coset</span>,
+  logo: (
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img src="/logo.svg" alt="Coset" style={{ width: 32, height: 32 }} />
+      <span style={{ marginLeft: "8px", fontWeight: "bold", fontSize: "24px" }}>Coset</span>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/CosetDev',
+    link: "https://github.com/CosetDev",
   },
   footer: {
-    text: 'Coset - Public Oracle Ecosystem',
+    text: "Coset - Public Oracle Ecosystem",
   },
-}
+};
 
-export default config
+export default config;
